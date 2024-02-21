c214d3c1a33111dbd7c3c6dd823d650cf25b4756
Max <maxnouts@gmail.com>
2024-02-21 19:05:37 +0100

    Set up Setting page + nav

---
+++ Personal/src/renderer/src/components/Navigation.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="containterNavigation">
+    <li>Home</li>
+    <li>Files</li>
+    <li>Settings</li>
+  </div>
+</template>
+
+<script setup>
+
+</script>
---
+++ Personal/src/renderer/src/components/Versions.vue
@@ -0,0 +1,34 @@
+<script>
+import Navigation from '../components/Navigation.vue';
+
+</script>
+<template>
+  <div class="ContainerSettings">
+    <Navigation />
+    <div class="ContainerSettingContent">
+      <h1>Settings</h1>
+      <p>Current theme source: <strong id="theme-source">System</strong></p>
+
+      <button id="toggle-dark-mode">Theme Switch</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue';
+
+const toggleDarkMode = async () => {
+  const isDarkMode = await window.darkMode.toggle();
+  document.getElementById('theme-source').innerHTML = isDarkMode ? 'Dark' : 'Light';
+};
+
+const resetToSystemTheme = async () => {
+  await window.darkMode.system();
+  document.getElementById('theme-source').innerHTML = 'System';
+};
+
+onMounted(() => {
+  document.getElementById('toggle-dark-mode').addEventListener('click', toggleDarkMode);
+  document.getElementById('reset-to-system').addEventListener('click', resetToSystemTheme);
+});
+</script>
